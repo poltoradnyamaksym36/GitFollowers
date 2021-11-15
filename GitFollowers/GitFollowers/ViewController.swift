67210fdef70084ea76df08ed0bd1c4e918414c67
Poltoradnya_Maksym <poltoradnyamaksym@gmail.com>
2021-11-15 13:03:30 +0200

    git init

---
+++ GitFollowers/GitFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by max36 on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
