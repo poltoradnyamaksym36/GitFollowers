2d54ed045052d235fffb06ce120595637f3c49a1
Poltoradnya_Maksym <poltoradnyamaksym@gmail.com>
2022-01-07 13:52:43 +0200

    no message

---
+++ GitFollowers/GitFollowers/GFTitleLabel.swift
@@ -0,0 +1,33 @@
+//
+//  GFTitleLabel.swift
+//  GitFollowers
+//
+//  Created by max36 on 01.01.2022.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlign: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
