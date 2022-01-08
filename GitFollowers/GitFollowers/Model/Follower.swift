7a93d3320cf1149a27030fcac21b18164720cd78
Poltoradnya_Maksym <poltoradnyamaksym@gmail.com>
2022-01-08 14:44:21 +0200

    создание моделей User, Follower

---
+++ GitFollowers/GitFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitFollowers
+//
+//  Created by max36 on 08.01.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
